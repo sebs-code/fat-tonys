910df1145d070a77755ea536e42f731ab878b466
Sebastian Lees <sebastian@incerto.net>
2025-07-27 22:38:53 +0100

    feature: book thumbs

---
+++ site/output/theme/books/thumbnails.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import glob, os
+
+size = 175, 265
+
+for infile in glob.glob("*.jpg"):
+    file, ext = os.path.splitext(infile)
+    with Image.open(infile) as im:
+        im.thumbnail(size)
+        im.save(file + "-thumbnail.jpg", "JPEG")
+        
+print('Created Thumbnails')
