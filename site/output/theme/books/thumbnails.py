cf34482d0940df6c7ab82909ad595f4022d4a619
Sebastian David Lees <sebastian@datablast.com>
2025-07-29 08:18:11 +0100

    bug: book sizes

--- site/output/theme/books/thumbnails.py
+++ site/output/theme/books/thumbnails.py
@@ -6,7 +6,10 @@ size = 175, 265
 for infile in glob.glob("*.jpg"):
     file, ext = os.path.splitext(infile)
     with Image.open(infile) as im:
-        im.thumbnail(size)
-        im.save(file + "-thumbnail.jpg", "JPEG")
+        im = im.resize(size)  # Force resize, ignore aspect ratio
+        if not file.endswith("-thumbnail"):
+            im.save(file + "-thumbnail.jpg", "JPEG")
+        else:
+             im.save(file + '.jpg', "JPEG")
         
 print('Created Thumbnails')
